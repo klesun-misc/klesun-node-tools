211a1d85ed45e22d2a4d6a7c62117662c0b13d90
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-06-20 13:11:23 +0000

    Add Misc.escapeXml()

--- src/Utils/Misc.js
+++ src/Utils/Misc.js
@@ -20,6 +20,17 @@ exports.chunk = (arr, size) => {
 
 exports.escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
 
+exports.escapeXml = (unsafe) =>
+	unsafe.replace(/[<>&'"]/g, (c) => {
+		switch (c) {
+			case '<': return '&lt;';
+			case '>': return '&gt;';
+			case '&': return '&amp;';
+			case '\'': return '&apos;';
+			case '"': return '&quot;';
+		}
+	});
+
 /**
  * @return RegExp
  *
