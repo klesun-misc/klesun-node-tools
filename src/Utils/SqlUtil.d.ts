dc44eababb91c17ab6fe806299dce77f86d8056e
Artur Klesun <30558426+klesun-misc@users.noreply.github.com>
2021-05-26 04:04:19 +0300

    Update SqlUtil.d.ts
    Add complete API typing

--- src/Utils/SqlUtil.d.ts
+++ src/Utils/SqlUtil.d.ts
@@ -1,17 +1,17 @@
 
 type operator = '=' | '>=' | '<=' | '!=';
 
+type PlacedValue = string | number;
+
 type condition =
     ['gds', '=', 'apollo'] |
     ['terminalNumber', '=', '2'] |
-    [string, operator, string | number] |
-    [string, 'IN', Array<string | number>] |
+    [string, operator, PlacedValue] |
+    [string, 'IN', Array<PlacedValue>] |
     [string, 'LIKE', string];
 
-export interface makeSelectQuery_rq {
+interface TargetedQueryBase {
     table: 'cmd_rq_log' | string;
-    as?: 'crl' | string;
-    fields?: Array<'gds' | string>;
     join?: Array<{
         type?: 'left' | string,
         table: 'terminal_command_log' | string,
@@ -21,10 +21,38 @@ export interface makeSelectQuery_rq {
     where?: condition[];
     whereOr?: condition[][];
     orderBy?: Array<
-        ['duration', 'ASC'] |
-        ['id', 'DESC'] |
-        [string, 'ASC' | 'DESC']
-    >;
+      ['duration', 'ASC'] |
+      ['id', 'DESC'] |
+      [string, 'ASC' | 'DESC']
+      >;
     skip?: '0' | number;
     limit?: '100' | number;
 }
+
+interface BuiltQuery {
+    sql: string,
+    placedValues: PlacedValue[],
+}
+
+export interface makeSelectQuery_rq extends TargetedQueryBase {
+    as?: 'crl' | string;
+    fields?: Array<'gds' | string>;
+}
+
+export declare const makeSelectQuery: (params: makeSelectQuery_rq) => BuiltQuery;
+
+export declare const makeInsertQuery: <TRow extends Record<string, PlacedValue>>(params: {
+    table: TargetedQueryBase['table'],
+    rows: TRow[],
+    insertType: 'insertOrUpdate' | 'replace' | 'insertOrUpdate' | 'insertNew',
+}) => BuiltQuery;
+
+export declare const makeUpdateQuery: (params: {
+    table: TargetedQueryBase['table'],
+    set: Record<string, PlacedValue>,
+    where: TargetedQueryBase['where'],
+}) => BuiltQuery;
+
+export declare const makeDeleteQuery: (params: TargetedQueryBase) => BuiltQuery;
+
+export declare const selectFromArray: <T>(params: makeSelectQuery_rq, allRows: T[]) => T[];
