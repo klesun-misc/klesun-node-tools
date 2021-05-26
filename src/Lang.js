318777e8924e288ca97f9b85aa381ab2f654ca05
Artur Klesun <30558426+klesun-misc@users.noreply.github.com>
2021-05-26 03:37:41 +0300

    Update Lang.js
    Remove functions that depended on removed now Rej.js

--- src/Lang.js
+++ src/Lang.js
@@ -1,36 +1,10 @@
 
-const Rej = require('./Rej.js');
-
 /**
  * this module provides core helper functions that extend the
  * language constructions, somehow similar to 'std' lib in C
  *
  * will mostly work with promises I guess
  */
-
-/**
- * @template T
- * @param {function(Error): T} defaultValueFunc
- * @param {{httpStatusCode}[]} allowedKinds - Rej.* http status codes
- * @return {function(Error): Promise<T>} - resolve if exc was created with Rej.* and it's
- *           status is in allowedStatuses, otherwise reject with original exc
- * supposed to be used with promise.catch(coverExc([Rej.NotFound], () => null)) to catch particular kinds of exceptions
- */
-exports.coverExc = (allowedKinds, defaultValueFunc = null) => {
-	defaultValueFunc = defaultValueFunc || (exc => null);
-	return (exc) => {
-		let allowedCodes = allowedKinds.map(r => r.httpStatusCode);
-		if (exc && allowedCodes.includes(exc.httpStatusCode)) {
-			return Promise.resolve()
-				.then(() => defaultValueFunc(exc));
-		} else {
-			return Promise.reject(exc);
-		}
-	};
-};
-
-/** handy when you need to filter a value in Promise chain */
-exports.nonEmpty = Rej.nonEmpty;
 
 /**
  * @param {Promise[]} promises
