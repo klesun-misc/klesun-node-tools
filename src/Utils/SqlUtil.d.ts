47fd4867b61be3b28521d2ff9594bbb639d933e5
Artur ECMA God Klesun <arturklesun@gmail.com>
2021-05-27 05:00:04 +0300

    Update typing for SqlUtil

--- src/Utils/SqlUtil.d.ts
+++ src/Utils/SqlUtil.d.ts
@@ -1,32 +1,35 @@
 
-type operator = '=' | '>=' | '<=' | '!=';
+type Operator = '=' | '>=' | '<=' | '!=' | '>' | '<';
 
-type PlacedValue = string | number;
+type PlacedValue = string | number | boolean;
+
+type ColumnName = string;
 
 type condition =
-    ['gds', '=', 'apollo'] |
-    ['terminalNumber', '=', '2'] |
-    [string, operator, PlacedValue] |
-    [string, 'IN', Array<PlacedValue>] |
-    [string, 'LIKE', string];
+    ['AND', condition[]] |
+    ['OR', condition[]] |
+    [string, Operator, PlacedValue] |
+    [string, 'IN' | 'NOT IN', Array<PlacedValue>] |
+    [string, 'IS' | 'IS NOT', null] |
+    [string, 'LIKE', string] |
+    BuiltQuery;
 
-interface TargetedQueryBase {
-    table: 'cmd_rq_log' | string;
+interface UntabledQueryBase {
     join?: Array<{
         type?: 'left' | string,
         table: 'terminal_command_log' | string,
         as: 'tcl' | string,
-        on: Array<['tcl.cmd_rq_id' | string, operator, 'crl.id' | string]>,
+        on: Array<['tcl.cmd_rq_id' | string, Operator, 'crl.id' | string]>,
     }>;
-    where?: condition[];
-    whereOr?: condition[][];
-    orderBy?: Array<
-      ['duration', 'ASC'] |
-      ['id', 'DESC'] |
-      [string, 'ASC' | 'DESC']
-      >;
+    where?: condition;
+    groupBy?: ColumnName[],
+    orderBy?: [ColumnName, 'ASC' | 'DESC'][];
     skip?: '0' | number;
     limit?: '100' | number;
+}
+
+interface TabledQueryBase extends UntabledQueryBase {
+    table: 'cmd_rq_log' | string;
 }
 
 interface BuiltQuery {
@@ -34,25 +37,28 @@ interface BuiltQuery {
     placedValues: PlacedValue[],
 }
 
-export interface makeSelectQuery_rq extends TargetedQueryBase {
+interface SelectSpecificFields {
     as?: 'crl' | string;
     fields?: Array<'gds' | string>;
 }
 
+export interface makeSelectQuery_rq extends TabledQueryBase, SelectSpecificFields {}
+export interface selectFromArray_rq extends UntabledQueryBase, SelectSpecificFields {}
+
 export declare const makeSelectQuery: (params: makeSelectQuery_rq) => BuiltQuery;
 
 export declare const makeInsertQuery: <TRow extends Record<string, PlacedValue>>(params: {
-    table: TargetedQueryBase['table'],
+    table: TabledQueryBase['table'],
     rows: TRow[],
-    insertType: 'insertOrUpdate' | 'replace' | 'insertOrUpdate' | 'insertNew',
+    insertType?: 'insertOrUpdate' | 'replace' | 'insertNew',
 }) => BuiltQuery;
 
 export declare const makeUpdateQuery: (params: {
-    table: TargetedQueryBase['table'],
+    table: TabledQueryBase['table'],
     set: Record<string, PlacedValue>,
-    where: TargetedQueryBase['where'],
+    where: TabledQueryBase['where'],
 }) => BuiltQuery;
 
-export declare const makeDeleteQuery: (params: TargetedQueryBase) => BuiltQuery;
+export declare const makeDeleteQuery: (params: TabledQueryBase) => BuiltQuery;
 
-export declare const selectFromArray: <T>(params: makeSelectQuery_rq, allRows: T[]) => T[];
+export declare const selectFromArray: <T>(params: selectFromArray_rq, allRows: T[]) => T[];
